fe6685a80838530979dfc3a3f4da6e3e6552fd04
v.novikov <v.novikov@involta.ru>
2022-06-05 18:46:33 +0400

    Initial Commit

---
+++ White and Fluffy by Vadim Novikov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  White and Fluffy by Vadim Novikov
+//
+//  Created by Vadim Novikov on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
